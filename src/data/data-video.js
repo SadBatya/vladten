5cebe46cb160c3773dfa6890722d0a3ba5cf6bdb
sadbatya <sadbatya@mail.ru>
2024-08-09 19:45:48 -0400

    fix data-video

--- src/data/data-video.js
+++ src/data/data-video.js
@@ -1,38 +1,38 @@
 export const dataVideoArr = [
   {
-    name: 'Топологическая сортировка и Minecraft - Зачем мне это надо от Влада Тен',
-    href: 'https://drive.google.com/file/d/1ox5CQItYg5qwW5XmbUZMVTw6vTa9StYE/preview'
+    name: "Топологическая сортировка и Minecraft - Зачем мне это надо от Влада Тен",
+    href: "https://drive.google.com/file/d/1ox5CQItYg5qwW5XmbUZMVTw6vTa9StYE/preview",
   },
   {
-    name: 'Влад и Фил решают Leetcode | Stack Overflow продали | Яндекс и Зелибоба | Прожектор Робапайка',
-    href: 'https://drive.google.com/file/d/1fiGAH1u6hwyh5xwBekucDktZrWJD7Zmx/preview'
+    name: "Влад и Фил решают Leetcode | Stack Overflow продали | Яндекс и Зелибоба | Прожектор Робапайка",
+    href: "https://drive.google.com/file/d/1fiGAH1u6hwyh5xwBekucDktZrWJD7Zmx/preview",
   },
   {
-    name: 'Влад Тен – Что такое девопс',
-    href: 'https://drive.google.com/file/d/178s03rtYoU_ugDLaeY_ZBp7Xk6q6xeaf/preview'
+    name: "Влад Тен – Что такое девопс",
+    href: "https://drive.google.com/file/d/178s03rtYoU_ugDLaeY_ZBp7Xk6q6xeaf/preview",
   },
   {
-    name: 'Влад Тен - Что произошло с OpenAI?',
-    href: 'https://drive.google.com/file/d/1FFUhHA6p8dunx-1J0KFTB1uA-Y0x8yul/preview'
+    name: "Влад Тен - Что произошло с OpenAI?",
+    href: "https://drive.google.com/file/d/1FFUhHA6p8dunx-1J0KFTB1uA-Y0x8yul/preview",
   },
   {
-    name: 'Система букинга и Priority Queue _ Зачем мне это нужно от Влада Тен',
-    href: 'https://drive.google.com/file/d/182FRuYT6-e6j8ydoU4obyXO0rC1nybOA/preview'
+    name: "Система букинга и Priority Queue _ Зачем мне это нужно от Влада Тен",
+    href: "https://drive.google.com/file/d/182FRuYT6-e6j8ydoU4obyXO0rC1nybOA/preview",
   },
   {
-    name: 'Как перестать играть в Доту_Кс _ Полезное видео от Влада Тен',
-    href: 'https://drive.google.com/file/d/1IQB-EF2Wq0y-HC3i3d2J2MvOhh4yKTG_/preview'
+    name: "Как перестать играть в Доту_Кс _ Полезное видео от Влада Тен",
+    href: "https://drive.google.com/file/d/1IQB-EF2Wq0y-HC3i3d2J2MvOhh4yKTG_/preview",
   },
   {
-    name: 'Linked List (Демо урок) - Влад Тен Алгоритмы',
-    href: 'https://drive.google.com/file/d/1O67S3tE0NCiChh_80SWtcjdNLdIyxjWK/preview'
+    name: "Linked List (Демо урок) - Влад Тен Алгоритмы",
+    href: "https://drive.google.com/file/d/1O67S3tE0NCiChh_80SWtcjdNLdIyxjWK/preview",
   },
   {
-    name: 'Почему распределенные системы это сложно_ Систем дизайн в Зачем мне это надо от Влада Тен_',
-    href: 'https://drive.google.com/file/d/1HXM3rUfs9zRht11_KkWNm8FPy1YN5JEG/preview'
-  }
+    name: "Почему распределенные системы это сложно_ Систем дизайн в Зачем мне это надо от Влада Тен_",
+    href: "https://drive.google.com/file/d/1HXM3rUfs9zRht11_KkWNm8FPy1YN5JEG/preview",
+  },
   {
-    name: 'Пишем свою Хэш Таблицу _ Курс по Алгоритмам от Влада Тен',
-    href: 'https://drive.google.com/file/d/1mbb5lk0FHZxy18K8MOzrxrgeIWYnXd9R/preview'
-  }
-]
+    name: "Пишем свою Хэш Таблицу _ Курс по Алгоритмам от Влада Тен",
+    href: "https://drive.google.com/file/d/1mbb5lk0FHZxy18K8MOzrxrgeIWYnXd9R/preview",
+  },
+];
